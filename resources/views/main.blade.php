9074c498affd89e877915156c66e6c288aba5b7d
Guilherme Lopes de Oliveira <guilhermelopes97@outlook.com>
2025-03-11 14:08:05 -0300

    004

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -1,16 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    
-    <h1>Welcome View and Blade</h1>
+@extends('layouts.main_layout')
 
+@section('content')
+    <h1>Welcome View and Blade</h1>
+    <hr>
     <h3>The value is: {{$value}}</h3>
     
-</body>
-</html>
+    <a href="/page2/{{$value}}">Page 2</a>
+    <br>
+    <a href="/page3/{{$value}}">Page 3</a>
+@endsection
+   
--- resources/views/page3.blade.php
+++ resources/views/page3.blade.php
@@ -1,17 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    
+@extends('layouts.main_layout')
+
+@section('content')
     <h1>Welcome View and Blade</h1>
     <hr>
     <h3>This is page 3</h3>
     <h3>The value is: {{$value}}</h3>
     
-</body>
-</html>
+    <a href="/main/{{$value}}">Main</a>
+    <br>
+    <a href="/page2/{{$value}}">Page 2</a>
+@endsection
+   
