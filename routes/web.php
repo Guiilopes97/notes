3c1bf02662551c3b0b225768a5e82a38cc09f2c2
Guilherme Lopes de Oliveira <guilhermelopes97@outlook.com>
2025-03-11 13:18:36 -0300

    001

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,10 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    // return view('welcome');
+    return "Hello World";
+});
+
+Route::get('/about', function () {
+    return "This is about page";
 });
