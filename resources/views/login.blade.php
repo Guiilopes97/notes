6218998ebcf18783d33821a3771c326921b1ac6b
Guilherme Lopes de Oliveira <guilhermelopes97@outlook.com>
2025-03-11 15:18:38 -0300

    009

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -18,11 +18,23 @@
                                 @csrf
                                 <div class="mb-3">
                                     <label for="text_username" class="form-label">Username</label>
-                                    <input type="text" class="form-control bg-dark text-info" id="text_username" name="text_username" autocomplete="username" required>
+                                    <input type="text" class="form-control bg-dark text-info" id="text_username" name="text_username" value="{{ old('text_username') }}" autocomplete="username" required>
+                                    {{-- show error --}}
+                                    @error('text_username')
+                                        <div class="text-danger">
+                                            <small>{{ $message }}</small>
+                                        </div>
+                                    @enderror
                                 </div>
                                 <div class="mb-3">
                                     <label for="text_password" class="form-label">Password</label>
-                                    <input type="password" class="form-control bg-dark text-info" id="text_password" name="text_password" autocomplete="current-password" required>
+                                    <input type="password" class="form-control bg-dark text-info" id="text_password" name="text_password" value="{{ old('text_password') }}" autocomplete="current-password" required>
+                                    {{-- show error --}}
+                                    @error('text_password')
+                                        <div class="text-danger">
+                                            <small>{{ $message }}</small>
+                                        </div>
+                                    @enderror
                                 </div>
                                 <div class="mb-3">
                                     <button type="submit" class="btn btn-secondary w-100">LOGIN</button>
@@ -36,17 +48,6 @@
                         <small>&copy; <?= date('Y') ?> Notes</small>
                     </div>
 
-                    {{-- errors--}}
-                    @if ($errors->any())
-                        <div class="alert alert-danger mt-3">
-                            <ul>
-                                @foreach ($errors->all() as $error)
-                                    <li>{{ $error }}</li>
-                                @endforeach
-                            </ul>
-                        </div>
-                    @endif
-                    
                 </div>
             </div>
         </div>
