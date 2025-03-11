0320faefece46f837514129db343a35d6164b2f5
Guilherme Lopes de Oliveira <guilhermelopes97@outlook.com>
2025-03-11 14:38:08 -0300

    007

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
-
+// Auth Routes
 Route::get('/login', [AuthController::class,'login']);
+Route::post('/loginSubmit', [AuthController::class,'loginSubmit']);
 Route::get('/logout', [AuthController::class,'logout']);
