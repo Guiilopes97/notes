9eb4eb64c3fea08c58f863b5638a0f5f4936bff4
Guilherme Lopes de Oliveira <guilhermelopes97@outlook.com>
2025-03-11 21:27:51 -0300

    023

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -13,7 +13,12 @@ class MainController extends Controller
     {
         // load user notes
         $id = session("user.id");
-        $notes = User::find($id)->notes()->orderBy('id', 'desc')->get()->toArray();
+        $notes = User::find($id)
+                            ->notes()
+                            ->whereNull('deleted_at')
+                            ->orderBy('id', 'desc')
+                            ->get()
+                            ->toArray();
 
         // show home view
         return view("home", ["notes"=> $notes]);
@@ -118,10 +123,39 @@ class MainController extends Controller
     public function deleteNote($id){
 
         $id = Operations::decryptId($id);
-        User::find(session("user.id"))->notes()->find($id)->delete();
-
-        return redirect()->route("home");
         
+        // load note
+        $note = Note::find($id);
+
+        // show delete note confirmation view
+        return view("delete_note", ["note" => $note]);
+        
+    }
+
+    public function deleteNoteConfirm($id){
+
+        // check if $id is encrypted
+        $id = Operations::decryptId($id);
+
+        // load note
+        $note = Note::find($id);
+
+        // 1. hard delete
+        // $note->delete();
+        
+        // 2. soft delete
+        // $note->deleted_at = date("Y-m-d H:i:s");
+        // $note->save();
+        
+        // 3. soft delete (propert in model)
+        $note->delete();
+
+        // 4. hard delete (propert in model)
+        // $note->forceDelete();
+
+        
+        // redirect to home
+        return redirect()->route("home");
     }
 
 }
--- resources/views/layouts/note.blade.php
+++ resources/views/layouts/note.blade.php
@@ -4,8 +4,20 @@
             <div class="row">
                 <div class="col">
                     <h4 class="text-info">{{ $note['title'] }}</h4>
-                    <small class="text-secondary"><span class="opacity-75 me-2">Created at:
-                        </span><strong>{{ date('d/m/Y H:i:s', strtotime($note['created_at'])) }}</strong></small>
+                    <small class="text-secondary"><span class="opacity-75 me-2">
+                        Created at:
+                        </span>
+                        <strong>{{ date('d/m/Y H:i:s', strtotime($note['created_at'])) }}</strong>
+                    </small>
+
+                    @if($note['updated_at'] != $note['created_at'])
+                        <small class="text-secondary ms-5"><span class="opacity-75 me-2">
+                            Updated at:
+                            </span>
+                            <strong>{{ date('d/m/Y H:i:s', strtotime($note['updated_at'])) }}</strong>
+                        </small>
+                    @endif
+                    
                 </div>
                 <div class="col text-end">
                     <a href="{{ route('edit', ['id' => encrypt($note['id'])]) }}" class="btn btn-outline-secondary btn-sm mx-1">
